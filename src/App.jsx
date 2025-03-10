ba00637758dc7db1c70a5c1d2dc117cb133f7e5f
Nick Goh <166708778+newmaldenite@users.noreply.github.com>
2025-03-10 23:17:45 +0000

    adds OAuth back in

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,47 @@ import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
+import UserLogo from './components/UserLogo/UserLogo';
+import { useState, useEffect } from 'react'
+import supabase from '../Helper/supabaseClient';
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+
 
 function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <div className="auth-container">
+        <Auth 
+          supabaseClient={supabase}
+          providers={["github"]}
+          appearance={{ theme: ThemeSupa }}
+          className="auth"
+        />
+      </div>
+    )
+  }
 
   return (
     <>
+      <UserLogo />
       <Header />
       <Main />
       <Footer />
